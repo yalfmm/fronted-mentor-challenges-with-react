3c38f8a156bd850be625b3602bd5a72ce048ebf2
yalfMM <yalfmm@gmail.com>
2023-10-05 08:10:20 -0400

    modificando lineas y moviendo carpeta de imagenes del proyecto 01

--- 01_results-summary-component/src/components/Data.jsx
+++ 01_results-summary-component/src/components/Data.jsx
@@ -4,18 +4,18 @@ import data from "../data.json";
 export default function Data() {
     const [items] = useState(data) 
     return(
-    <div className=''>      
-      {items.map((item, index) => (
-        <div key={index} style={{backgroundColor: item.bgColor,}} className='my-3 rounded-xl flex py-5 px-6 items-center'>
-          <img src={item.icon} alt={item.category} className='' />
-          <p style={{color: item.color,}} className=''>
-            <p className='ml-3 text-lg'>{item.category}</p>
-          </p>
-          <p className='ml-auto font-bold text-ntl-grayblue'>
-            {item.score}
-            <span className='text-ntl-grayblue/70'>/ 100</span>
-          </p>
-        </div>
-      ))}
-    </div>
+      <div className=''>      
+        {items.map((item, index) => (
+          <div key={index} style={{backgroundColor: item.bgColor,}} className='my-3 rounded-xl flex py-5 px-6 items-center'>
+            <img src={`/images/${item.icon}`} alt={item.category} className='' />
+            <p style={{color: item.color,}} className=''>
+              <p className='ml-3 text-lg'>{item.category}</p>
+            </p>
+            <p className='ml-auto font-bold text-ntl-grayblue'>
+              {item.score}
+              <span className='text-ntl-grayblue/70'>/ 100</span>
+            </p>
+          </div>
+        ))}
+      </div>
 )}
